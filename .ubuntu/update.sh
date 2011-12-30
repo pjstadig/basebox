5fdefea62d11b7fb7431cf9c93d69c3ca8ef7efb
Paul Stadig <paul@stadig.name>
2011-12-30 13:04:32 -0500

    Configure not to install recommended packages.

--- .ubuntu/update.sh
+++ .ubuntu/update.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -eux
 
+echo "APT::Install-recommends \"no\";" >/tmp/00no-install-recommends
+install /tmp/00no-install-recommends /etc/apt/apt.conf.d/00no-install-recommends -m0644 -oroot -groot
+rm /tmp/00no-install-recommends
+
 apt-get update
 apt-get -y upgrade
